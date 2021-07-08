effa6619fe36eb3f2bd2fbb9d58bbf795c819a68
khondamir <khondamir.anvarov@gmail.com>
2021-07-08 04:21:23 -0700

    vsftpd installation
    Added first functionaluty to the installer
    vsftpd may be installed in 2 modes
    other minor modification features as open .conf file
    reset settings
    backup settings
    check status of server
    turn server ON/OFF
    open .conf file

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-MY_SHELL="korn"
-IFCNF=$(ifconfig)
-echo "I like the $MY_SHELL shell"
-echo "ifconfig is $IFCNF"
+
+# MY_SHELL="korn"
+# IFCNF=$(ifconfig)
+# echo "I like the $MY_SHELL shell"
+# echo "ifconfig is $IFCNF"
+
+for FL in $@
+do
+([ -f "$FL" ] &&  echo "$FL is regular file" ) || ([ -d "$FL" ] &&  echo "$FL is directory")  ||  echo "Your file is something irregular $FL"
+done
